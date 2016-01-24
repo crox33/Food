026608a53def3d501db7bff3082629ecd0290315
wmyap <weimunyap@weis-macbook-pro.local>
2016-01-24 17:14:27 +0000

    Initial Commit

---
+++ Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Wei Mun Yap on 24/01/2016.
+//  Copyright © 2016 UrbanVillage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
